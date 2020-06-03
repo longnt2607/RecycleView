e22babc321d06fea295e86c711e2f1004e9a3e16
longnt2607 <ntlong12c1ncthanh@gmail.com>
2020-06-03 10:36:18 +0700

    commit so 3

--- app/src/main/java/vn/edu/ntu/nguyentruonglong/recycleview/ShoppingCartActivity.java
+++ app/src/main/java/vn/edu/ntu/nguyentruonglong/recycleview/ShoppingCartActivity.java
@@ -3,7 +3,10 @@ package vn.edu.ntu.nguyentruonglong.recycleview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -14,13 +17,18 @@ public class ShoppingCartActivity extends AppCompatActivity {
 
     TextView txtShoppingCart;
     ICartController controller;
+    Button btnXoa;
+    Button btnMua;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
         controller = (ICartController) getApplication();
         addView();
+        addEvent();
     }
+
     private void addView() {
         txtShoppingCart = findViewById(R.id.txtShoppingCart);
         displayShoppingCart();
@@ -42,5 +50,31 @@ public class ShoppingCartActivity extends AppCompatActivity {
         else {
             txtShoppingCart.setText("Khong co mat hang nao trong gio hang");
         }
+    }
+
+    private void addEvent() {
+        btnXoa = findViewById(R.id.btnCancle);
+        btnXoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                controller.clearShoppingCart();
+                Toast.makeText(getApplicationContext(), "Xoa gio hang thanh cong.", Toast.LENGTH_SHORT);
+                displayShoppingCart();
+            }
+        });
+
+        btnMua = findViewById(R.id.btnBuy);
+        btnMua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Product> list;
+                list = controller.getShoppingCart();
+                int sum = 0;
+                for (Product p : list) {
+                    sum += p.getPrice();
+                }
+                Toast.makeText(getApplicationContext(), "Tong gia tri cua gio hang: " + sum + " Vnd.", Toast.LENGTH_SHORT);
+            }
+        });
     }
 }
